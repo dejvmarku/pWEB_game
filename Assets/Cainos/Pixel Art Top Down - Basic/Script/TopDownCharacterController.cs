dbdb25559a9958d19e81a193fdc563327f827502
dejvmarku <79286844+dejvmarku@users.noreply.github.com>
2021-03-09 16:15:38 +0100

    UnityGraph

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -19,7 +19,7 @@ namespace Cainos.PixelArtTopDown_Basic
         private void Update()
         {
             Vector2 dir = Vector2.zero;
-            if (Input.GetKey(KeyCode.M))
+            if (Input.GetKey(KeyCode.U))
             {
                 dir.x = -1;
                 animator.SetInteger("Direction", 3);
